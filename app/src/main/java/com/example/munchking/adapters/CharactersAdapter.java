8752c83dbc12d500a03487ee95daa6375ae7e42f
Jonathan Gomez <jondavgo@fb.com>
2020-07-14 17:17:03 -0400

    Created Character class, item view, and adapter for RecyclerView.

---
+++ app/src/main/java/com/example/munchking/adapters/CharactersAdapter.java
@@ -0,0 +1,74 @@
+package com.example.munchking.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.munchking.R;
+import com.example.munchking.models.Character;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+public class CharactersAdapter extends RecyclerView.Adapter<CharactersAdapter.ViewHolder> {
+
+    List<Character> characters;
+    Context context;
+
+    public CharactersAdapter(List<Character> characters, Context context) {
+        this.characters = characters;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item_character, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Character character = characters.get(position);
+        holder.bind(character);
+    }
+
+    @Override
+    public int getItemCount() {
+        return characters.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView ivPhoto;
+        TextView tvName;
+        TextView tvTtrpg;
+        TextView tvUser;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivPhoto = itemView.findViewById(R.id.ivPhoto);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvTtrpg = itemView.findViewById(R.id.tvTtrpg);
+            tvUser = itemView.findViewById(R.id.tvUser);
+        }
+
+        public void bind(Character character) {
+            tvName.setText(character.getName());
+            tvTtrpg.setText(character.getTtrpg());
+            tvUser.setText(character.getUser().getUsername());
+            ParseFile photo = character.getPhoto();
+            if(photo != null) {
+                Glide.with(context).load(photo.getUrl()).into(ivPhoto);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/munchking/models/Character.java
@@ -0,0 +1,53 @@
+package com.example.munchking.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Character")
+public class Character extends ParseObject {
+
+    // Keys
+    public static final String KEY_NAME = "name";
+    public static final String KEY_PHOTO = "photo";
+    public static final String KEY_TTRPG = "ttrpg";
+    public static final String KEY_USER = "creator";
+
+    // Required empty constructor
+    public Character(){}
+
+    // Getters
+    public String getName() {
+        return getString(KEY_NAME);
+    }
+
+    public ParseFile getPhoto() {
+        return getParseFile(KEY_PHOTO);
+    }
+
+    public String getTtrpg() {
+        return getString(KEY_TTRPG);
+    }
+
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    // Setters
+    public void setName(String name){
+        put(KEY_NAME, name);
+    }
+
+    public void setPhoto(ParseFile photo){
+        put(KEY_PHOTO, photo);
+    }
+
+    public void setTtrpg(String ttrpg){
+        put(KEY_TTRPG, ttrpg);
+    }
+
+    public void setUser(ParseUser user){
+        put(KEY_USER, user);
+    }
+}
