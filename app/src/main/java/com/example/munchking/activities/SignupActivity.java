863d3bc2a112837c1ab91a716af17aedc54d294f
Jonathan Gomez <jondavgo@fb.com>
2020-08-10 16:36:45 -0400

    Added a system to make sure entered emails use a valid email format of "___@___.___"

--- app/src/main/java/com/example/munchking/activities/SignupActivity.java
+++ app/src/main/java/com/example/munchking/activities/SignupActivity.java
@@ -1,14 +1,13 @@
 package com.example.munchking.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.munchking.R;
 import com.google.android.material.textfield.TextInputEditText;
@@ -47,7 +46,18 @@ public class SignupActivity extends AppCompatActivity {
                 String email = etEmail.getText().toString();
                 String confirm = etConfirm.getText().toString();
                 if(password.equals(confirm)) {
-                    signUp(username, password, email);
+                    int i = email.indexOf("@");
+                    String afterAt;
+                    if (i > 0) {
+                        afterAt = email.substring(i);
+                    } else {
+                        afterAt = "";
+                    }
+                    if (afterAt.contains(".")) {
+                        signUp(username, password, email);
+                    } else {
+                        Toast.makeText(SignupActivity.this, R.string.invalid_email, Toast.LENGTH_SHORT).show();
+                    }
                 } else {
                     Toast.makeText(SignupActivity.this, R.string.password_mismatch, Toast.LENGTH_SHORT).show();
                 }
